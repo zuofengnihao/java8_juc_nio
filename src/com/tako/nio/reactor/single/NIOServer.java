bd710e3b7f28101a2c2838588abea837fcd9a31f
zuofeng <zuofengnihao@163.com>
2020-04-16 16:56:23 +0800

    a

--- src/com/tako/nio/reactor/single/NIOServer.java
+++ src/com/tako/nio/reactor/single/NIOServer.java
@@ -2,6 +2,7 @@ package com.tako.nio.reactor.single;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -23,7 +24,7 @@ public class NIOServer {
             serverSocketChannel.bind(new InetSocketAddress(port));
 
             Selector selector = Selector.open();
-            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT | SelectionKey.OP_READ | SelectionKey.OP_READ);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
             while (selector.select() > 0) {
                 Iterator<SelectionKey> it = selector.selectedKeys().iterator();
@@ -34,10 +35,22 @@ public class NIOServer {
                         socketChannel.configureBlocking(false);
                         socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
                     } else if (key.isReadable()) {
-
-                    } else if (key.isWritable()) {
-
-                    }
+                        ByteBuffer buffer = ByteBuffer.allocate(1024);
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        int len = 0;
+                        while ((len = channel.read(buffer)) > 0) {
+                            buffer.flip();
+                            System.out.print(new String(buffer.array(), 0 ,len));
+                            buffer.clear();
+                        }
+                    } /*else if (key.isWritable()) {
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(1024);
+                        buffer.put("hello".getBytes());
+                        buffer.flip();
+                        channel.write(buffer);
+                    }*/
+                    it.remove();
                 }
             }
 
